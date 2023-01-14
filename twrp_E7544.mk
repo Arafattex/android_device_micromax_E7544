4510a0a30a6ec21b37183688d57f0ec4b3dcde02
Arafattex <69374795+Arafattex@users.noreply.github.com>
2023-01-14 18:55:43 +0600

    Add for pb

--- twrp_E7544.mk
+++ twrp_E7544.mk
@@ -10,8 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common pbrp stuff.
+$(call inherit-product, vendor/pbrp/config/common.mk)
 
 # Inherit from E7544 device
 $(call inherit-product, device/micromax/E7544/device.mk)
